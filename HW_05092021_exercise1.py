1d2afc530ff955ad338ed0db9f94e51c4579e216
Max M. Steiner <steinermax2019@gmail.com>
2021-09-11 17:27:17 +0300

    Homework

---
+++ HW_05092021_exercise1.py
@@ -0,0 +1,9 @@
+# 1. input a number check if it is divided by 10 without reminder
+#    you need to check if it is % 5 and % 2 == 0, if so print ('can be divided by 10')
+
+a = float(input("Enter any number "))
+if a % 2 == 0 and a % 5 == 0:
+    print("The number you entered is divisible by 10")
+else:
+   print("The number you entered isn't divisible by 10")
+
---
+++ HW_05092021_exercise3.py
@@ -0,0 +1,10 @@
+#3. input a number from the user
+#   check if the number has 2 digits, if so print the ahadot digit
+#    you MUST use 1 if (+and)
+
+a = float(input("Enter any number: "))
+a = abs(a)
+if abs(a / 10) >= 1 and abs(a / 10) < 10:
+    print("number of units in the number: ", a % 10)
+else:
+    print("this is not a two-digit number")
