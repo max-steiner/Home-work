561e757e9a9338ccfee8b29ea6ab6593306245db
Max M. Steiner <steinermax2019@gmail.com>
2021-09-11 18:09:48 +0300

    HW

--- HW_05092021_exercise1.py
+++ HW_05092021_exercise1.py
@@ -2,6 +2,7 @@
 #    you need to check if it is % 5 and % 2 == 0, if so print ('can be divided by 10')
 
 a = float(input("Enter any number "))
+a = abs(a)
 if a % 2 == 0 and a % 5 == 0:
     print("The number you entered is divisible by 10")
 else:
